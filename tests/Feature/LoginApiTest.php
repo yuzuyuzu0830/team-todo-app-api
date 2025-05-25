76edd4706a465c2fef5fff2853ee1cbf785cd794
yuzuyuzu0830 <kiki.balon@gmail.com>
2025-05-25 23:15:03 +0900

    loginのテストコード作成

---
+++ tests/Feature/LoginApiTest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class LoginApiTest extends TestCase
+{
+    use RefreshDatabase;
+    
+    public function test_login_successfully() {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(['token']);
+            
+    }
+
+    public function test_login_with_wrong_password() {
+        $user = User::factory()->create([
+            'email' => 'test@example.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        $response = $this->postJson('/api/login', [
+            'email' => 'test@example.com',
+            'password' => 'wrong_password',
+        ]);
+        
+        $response->assertStatus(422);
+
+        $response->assertJson([
+            'message' => '認証に失敗しました',
+            'errors' => [
+                'email' => ['認証に失敗しました']
+            ]
+        ]);
+    }
+}
