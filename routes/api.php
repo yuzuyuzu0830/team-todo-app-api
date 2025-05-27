15fdcd11f189aa5aa10044815517f2ae1c71b528
yuzuyuzu0830 <kiki.balon@gmail.com>
2025-05-27 21:53:51 +0900

    ログインメソッド修正

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\Api\TodoController;
 
 Route::post('/login', [LoginController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/todos', [TodoController::class, 'index']);
-
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/todos', [TodoController::class, 'index']);
+    Route::post('/todos', [TodoController::class, 'create']);
+});
